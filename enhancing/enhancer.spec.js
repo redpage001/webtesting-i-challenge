6fa863849372ebfaf3e48048823906cbbe004dd5
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-05-20 12:33:13 -0700

    Added Jst and created first failed test.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,7 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancer', () => {
+    it("should take an object and return the same opject with a durability property of 100", () => {
+        expect(enhancer.repair({ durability: 5 })).toBe({ durability: 100 })
+    })
+})
