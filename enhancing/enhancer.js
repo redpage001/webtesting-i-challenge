435393158ddd737739ad91a42f59875486143a11
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-05-20 14:56:08 -0700

    Created tests and functions for repair, succeed and fail operations.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,56 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item && item.name && item.durability && item.enhancement){
+    const { enhancement } = item;
+
+    if(enhancement < 20 && enhancement > -1) {
+      return {...item, enhancement: enhancement + 1};
+    } else if (enhancement === 20) {
+      return { message: "Item cannot be enhanced any further." }
+    } else {
+      return { message: "Nice hacked weapon, cheater." }
+    }
+  } else {
+    return null;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item && item.name && item.durability && item.enhancement){
+    const { enhancement } = item;
+
+    if(enhancement > 16 && enhancement < 20) {
+      return { ...item, enhancement: enhancement - 1};
+    } else if (enhancement > 14 && enhancement < 17) {
+      return { ...item, enhancement: enhancement - 10};
+    } else if (enhancement > 4 && enhancement < 15) {
+      return { ...item, enhancement: enhancement - 5};
+    } else if (enhancement > -1 && enhancement < 5) {
+      return { ...item, enhancement: 0};
+    } else if (enhancement === 20) {
+      return { message: "Item cannot be enhanced any further." }
+    } else {
+      return { message: "Nice hacked weapon, cheater." }
+    }
+  } else {
+    return null;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if(item && item.name && item.durability && item.enhancement){
+    const { durability } = item;
+    if(durability < 100 && durability > -1) {
+      return { ...item, durability: 100 };
+    } else if(durability === 100) {
+      return { message: "Item is in perfect condition." }
+    } else {
+      return { message: "Nice hacked weapon, cheater." }
+    }
+  } else {
+    return null;
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,53 @@
 const enhancer = require('./enhancer.js');
 // test away!
+const array = [];
+const emptyObject = {};
+const weaponName = { name: "sword" };
+const brokenWeapon = { name: "iron sword", durability: 5, enhancement: 5 };
+const perfectWeapon = { name: "Holy Sword", durability: 100, enhancement: 20 };
+const hackedWeapon = { name: "Hacked Sword", durability: 125, enhancement: 25 };
+const negativeWeapon = { name: "Negative Weapon", durability: -1, enhancement: -1 };
+
 describe('enhancer', () => {
     it("should take an object and return the same opject with a durability property of 100", () => {
-        expect(enhancer.repair({ durability: 5 })).toBe({ durability: 100 })
+        
+        expect(enhancer.repair(brokenWeapon)).toStrictEqual({ ...brokenWeapon, durability: 100 });
+        expect(enhancer.repair(perfectWeapon)).toStrictEqual({ message: "Item is in perfect condition."});
+        expect(enhancer.repair(hackedWeapon)).toStrictEqual({ message: "Nice hacked weapon, cheater."});
+        expect(enhancer.repair(negativeWeapon)).toStrictEqual({ message: "Nice hacked weapon, cheater."});
+        expect(enhancer.repair(weaponName)).toBe(null);
+        expect(enhancer.repair(array)).toBe(null);
+        expect(enhancer.repair(emptyObject)).toBe(null);
+        expect(enhancer.repair()).toBe(null);
+    })
+
+    it("should increase enhancement level by 1 if enchantment level is less than 20", () => {
+
+        expect(enhancer.succeed(brokenWeapon)).toStrictEqual({ ...brokenWeapon, enhancement: brokenWeapon.enhancement + 1})
+        expect(enhancer.succeed(perfectWeapon)).toStrictEqual({ message: "Item cannot be enhanced any further." });
+        expect(enhancer.succeed(hackedWeapon)).toStrictEqual({ message: "Nice hacked weapon, cheater."});
+        expect(enhancer.succeed(negativeWeapon)).toStrictEqual({ message: "Nice hacked weapon, cheater."});
+        expect(enhancer.succeed(weaponName)).toBe(null);
+        expect(enhancer.succeed(array)).toBe(null);
+        expect(enhancer.succeed(emptyObject)).toBe(null);
+        expect(enhancer.succeed()).toBe(null);
+    })
+
+    it("should decreace enhancement level by 5 if less than 15, by 10 if 15 or 16, by 1 if greater than 16", () => {
+        const testWeapon1 = { name: "testWeapon1", durability: 100, enhancement: 10 };
+        const testWeapon2 = { name: "testWeapon2", durability: 100, enhancement: 15 };
+        const testWeapon3 = { name: "testWeapon3", durability: 100, enhancement: 17 }
+        
+        expect(enhancer.fail(brokenWeapon)).toStrictEqual({ ...brokenWeapon, enhancement: 0 });
+        expect(enhancer.fail(testWeapon1)).toStrictEqual({ ...testWeapon1, enhancement: 5 });
+        expect(enhancer.fail(testWeapon2)).toStrictEqual({ ...testWeapon2, enhancement: 5 });
+        expect(enhancer.fail(testWeapon3)).toStrictEqual({ ...testWeapon3, enhancement: 16 });
+        expect(enhancer.fail(perfectWeapon)).toStrictEqual({ message: "Item cannot be enhanced any further." });
+        expect(enhancer.fail(hackedWeapon)).toStrictEqual({ message: "Nice hacked weapon, cheater."});
+        expect(enhancer.fail(negativeWeapon)).toStrictEqual({ message: "Nice hacked weapon, cheater."});
+        expect(enhancer.fail(weaponName)).toBe(null);
+        expect(enhancer.fail(array)).toBe(null);
+        expect(enhancer.fail(emptyObject)).toBe(null);
+        expect(enhancer.fail()).toBe(null);
     })
 })
